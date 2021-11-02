dfdbd81a92f568aa6a5fccb13e56dfd8eafc8d19
Holivane <holivane@gmail.com>
2021-11-02 12:11:44 -0300

    Criando estrutura de objeto

---
+++ curso_javascript/objeto/objeto.js
@@ -0,0 +1,9 @@
+const listaCPFs = ['12365242625', '9242624265']
+// const cliente = ['nome', 'André','idade','36']
+
+const cliente = {
+    nome: 'André'
+    idade: 36
+    cpf: 12546825933
+    email: andre@email.com
+}
