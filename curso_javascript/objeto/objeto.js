8a0f36eab911c3e402ca9ce5e6f42572d9b510a3
Holivane <holivane@gmail.com>
2021-11-02 12:17:58 -0300

    Printando objetos

--- curso_javascript/objeto/objeto.js
+++ curso_javascript/objeto/objeto.js
@@ -2,8 +2,11 @@ const listaCPFs = ['12365242625', '9242624265']
 // const cliente = ['nome', 'André','idade','36']
 
 const cliente = {
-    nome: 'André'
-    idade: 36
-    cpf: 12546825933
-    email: andre@email.com
+    nome:'Holivane',
+    idade:33,
+    cpf:'12546825933',
+    email: 'holivane@email.com',
 }
+
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade} anos.`)
+console.log(`Meu CPF é: ${cliente.cpf.substring(0,3)}.***.***-**`)
